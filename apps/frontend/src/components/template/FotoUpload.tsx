152a38883cac4b8dcccd93dabb420d86445a09f6
NayanaLoivos <loivosdev@gmail.com>
2024-12-06 23:07:00 -0300

    Add FotoUpload component

---
+++ apps/frontend/src/components/template/FotoUpload.tsx
@@ -0,0 +1,9 @@
+import Image from 'next/image';
+
+export default function FotoUpload() {
+    return (
+        <div>
+            <Image src="/Dog-Foto.svg" width={50} height={50} alt="inserir foto" />
+        </div>
+    )
+}
