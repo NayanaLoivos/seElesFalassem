0a7a0b9d44f6c3dc3ed2e8c8a9baaf849c51533b
NayanaLoivos <loivosdev@gmail.com>
2024-12-26 21:39:51 -0300

    configed default color for application

--- apps/frontend/src/components/template/FotoUpload.tsx
+++
@@ -1,9 +0,0 @@
-import Image from 'next/image';
-
-export default function FotoUpload() {
-    return (
-        <div>
-            <Image src="/Dog-Foto.svg" width={50} height={50} alt="inserir foto" />
-        </div>
-    )
-}
