279b0bba37d381ae662685c3a75ee1a7d7dfc37b
NayanaLoivos <loivosdev@gmail.com>
2024-12-28 19:04:03 -0300

    Added loading icon

--- apps/frontend/src/components/template/PageLoading.tsx
+++ apps/frontend/src/components/template/PageLoading.tsx
@@ -12,12 +12,16 @@ export default function PageLoading(props: PaginaProps) {
         <div className="">
             <Logo />
             <main className={ props.className }>{ props.children }
-                <div className= "flex items-center justify-center">
-                    <Image className="h-screen"
-                        src="/Tela-carregamento.svg" width={200} height={200} alt="logo"/>
+                <div className= "flex flex-col items-center justify-center">
+                    <Image className=""
+                        src="/Tela-carregamento.svg" width={400} height={400} alt="logo"/>
+                    <Image className="py-5"
+                           src="/icones/loader.svg" width={40} height={40} alt="logo"/>
                 </div>
-                <footer className="text-center "> A ferramenta de informações que você precisava para lembrar de tudo sobre seu Pet a qualquer hora!</footer>
-
+                <div>
+                    <footer className="text-center"> A ferramenta que coloca as informações do seu pet na palma da sua mão!
+                    </footer>
+                </div>
             </main>
 
 
